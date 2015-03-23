556d0224ae4394bd2d11d9a574e4d65f9b1f9410
chuguangming <chu888chu888@qq.com>
2015-03-23 17:07:39 +0800

    整理目录

--- 抽屉菜单/未命名文件夹/CustomeCellTable/MSMenuCell.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-@interface MSMenuCell : UITableViewCell
-@property (nonatomic) UIImageView *sideCellImageView;
-@property (nonatomic) UILabel *sideCellLab;
-@end
